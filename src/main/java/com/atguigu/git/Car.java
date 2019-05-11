18959b09d3ce74a3e1f84b86a00bb95160475b61
lianggq <578021502@qq.com>
2019-05-11 16:59:47 +0800

    I like COROLLA...

--- src/main/java/com/atguigu/git/Car.java
+++ src/main/java/com/atguigu/git/Car.java
@@ -9,5 +9,6 @@ public class Car {
 		System.out.println("My name is TestGit2");
 		System.out.println("My name is TestGit");
 		System.out.println("Test Git hot_fix");
+		System.out.println("I like COROLLA...");
 	}
 }
